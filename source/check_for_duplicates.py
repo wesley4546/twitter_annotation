5e98717b9d713779f5806d9c62dc872b0cf6d0fa
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-08-04 16:28:23 -0400

    added better errors

--- source/check_for_duplicates.py
+++ source/check_for_duplicates.py
@@ -13,14 +13,18 @@ def check_for_duplicates(annotation_csv, output_csv):
     try:
         # Removes NAs
         ant_csv = pd.read_csv(annotation_csv).dropna(how='all', subset=['ScreenName'])
-        out_csv = pd.read_csv(output_csv)
-
-        # Gets the usernames that already are done
-        out_csv_usernames = out_csv['twitter_username'].to_list()
-
-        # Removes them from the csv
-        consolidated_csv = ant_csv[~ant_csv['ScreenName'].isin(out_csv_usernames)]
-
-        return consolidated_csv
     except:
-        print("ERROR: Problem with column names. Make sure the column names are `ScreenName` and `URL`")
+        print("ERROR: Problem with column names of `input_csv`. Make sure the column names are `ScreenName` and `URL`")
+
+    try:
+        out_csv = pd.read_csv(output_csv)
+    except:
+        print("ERROR: Problem with reading `output_csv`.")
+
+    # Gets the usernames that already are done
+    out_csv_usernames = out_csv['twitter_username'].to_list()
+
+    # Removes them from the csv
+    consolidated_csv = ant_csv[~ant_csv['ScreenName'].isin(out_csv_usernames)]
+
+    return consolidated_csv
